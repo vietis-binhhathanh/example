b9e5f7a2db350c86c30a6e60a11d748fbf4c9b34
thanh-binh <binh.hathanh@vietis.com.vn>
2020-10-24 14:48:09 +0700

    __________

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,12 +11,19 @@ const routes: RouteConfig[] = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/Login.vue'),
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('@/views/Register.vue'),
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: () => import('@/views/Setting.vue'),
   },
 ];
 
